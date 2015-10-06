68883fbfab972891f5a762225ad4b1e74d2b794d
xiaosonl <xiaosonl@gmail.com>
2015-10-06 18:00:42 +0800

    Commit

--- README.md
+++ README.md
@@ -8,14 +8,20 @@ Node.js OAuth Library used RSA<br/>
 var RSAOAuth = require('rsa-oauth');
 console.log(RSAOAuth);
 var oauth = new RSAOAuth({
-    expired : 30 * 60
+    expired : 30 * 60   
 });
+oauth.loadPrivateKey();
+var oauth2 = new RSAOAuth();
+oauth2.loadPublicKey();
+
 var token = oauth.createToken();
-console.log("New Token: " + token);
-console.log("The Token is " + oauth.getToken(token).isExpired);
-var tokenID = oauth.createTokenID(1235234);
-console.log("New TokenID: " + tokenID);
-console.log("The ID is " + oauth.getTokenID(tokenID));
+console.log("The Token is:" + token);
+console.log("The TokenObj is: ", JSON.stringify(oauth2.getToken(token)));
+
+var tokenID = oauth.createTokenID(123);
+console.log("The TokenID is : ", tokenID);
+console.log("The ID is :", oauth2.getTokenID(tokenID));
+
 ```
 
 ## Installing
--- src/RSAOAuth.js
+++ src/RSAOAuth.js
@@ -5,50 +5,53 @@
 var NodeRSA = require('node-rsa')
     ,fs = require("fs");
 //Init RSA
-var key = new NodeRSA({b: 512 });
 var public_key_name = 'pkcs1-public.key';
 var private_key_name = 'pkcs1-private.key';
-var public_key_data = '', private_key_data ='';
-
-//Init Public key
-if(fs.existsSync(public_key_name)){
-    public_key_data = fs.readFileSync(public_key_name);
-    key.importKey(public_key_data, "pkcs1-public");
-    console.log("import public key from file:" + public_key_name);
-}else{
-    public_key_data = key.exportKey('pkcs1-public');
-    console.log("create public key file:" + public_key_name);
-    fs.writeFileSync(public_key_name, public_key_data)
-}
-console.log("public_key_data is : ");
-console.log(public_key_data.toString());
-
-//Init Private Key
-if(fs.existsSync(private_key_name)){
-    private_key_data = fs.readFileSync(private_key_name);
-    key.importKey(private_key_data, "pkcs1-private")
-    console.log("import private key from file:" + private_key_name);
-}else{
-    private_key_data = key.exportKey('pkcs1-private');
-    console.log("create private key file:" + private_key_name);
-    fs.writeFileSync(private_key_name, private_key_data)
-}
-console.log("private_key_data is:");
-console.log(private_key_data.toString());
 
 module.exports = (function(){
     function RSAOAuth(options){
-        RSAOAuth.prototype.options = options;
+        this.options = options;
+        this.key = new NodeRSA({b : 512});
+    }
+
+    RSAOAuth.prototype.loadPublicKey = function(){
+        var public_key_data = '';
+        if(fs.existsSync(public_key_name)){
+            public_key_data = fs.readFileSync(public_key_name);
+            this.key.importKey(public_key_data, "pkcs1-public");
+            console.log("import public key from file:" + public_key_name);
+        }else{
+            public_key_data = this.key.exportKey('pkcs1-public');
+            console.log("create public key file:" + public_key_name);
+            fs.writeFileSync(public_key_name, public_key_data)
+        }
+        console.log(public_key_data.toString());
+    }
+
+    RSAOAuth.prototype.loadPrivateKey = function(){
+        var private_key_data ='';
+        if(fs.existsSync(private_key_name)){
+            private_key_data = fs.readFileSync(private_key_name);
+            this.key.importKey(private_key_data, "pkcs1-private")
+            console.log("import private key from file:" + private_key_name);
+        }else{
+            private_key_data = this.key.exportKey('pkcs1-private');
+            console.log("create private key file:" + private_key_name);
+            fs.writeFileSync(private_key_name, private_key_data)
+        }
+        console.log(private_key_data.toString());
     }
 
     RSAOAuth.prototype.createToken = function (state) {
         var token = {
             createTime: new Date(),
-            expired: this.options.expired,
             state: state
         }
+        if(this.options){
+            token.expired = this.options.expired;
+        }
         var tokenJson = JSON.stringify(token);
-        return key.encrypt(tokenJson, "base64").toString();
+        return this.key.encryptPrivate(tokenJson, "base64").toString();
     }
 
     RSAOAuth.prototype.createTokenID = function (id) {
@@ -65,9 +68,9 @@ module.exports = (function(){
     }
 
     RSAOAuth.prototype.getToken = function(token) {
-        var tokenJson = key.decrypt(token, 'utf-8');
+        var tokenJson = this.key.decryptPublic(token, 'utf-8');
         var tokenObj = JSON.parse(tokenJson);
-        if (tokenObj && tokenObj.createTime && tokenObj.expired) {
+        if (tokenObj && tokenObj.createTime) {
             var now = new Date();
             var createTime = new Date(tokenObj.createTime);
             var seconds = (now - createTime) / 1000;
